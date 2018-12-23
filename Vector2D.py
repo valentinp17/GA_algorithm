43f4028a1d0d9ba292e690ef86e4850c0cccd9c8
Valentin <pvv377@gmail.com>
2018-12-23 08:38:58 +0300

    Added class Vector2D

---
+++ Vector2D.py
@@ -0,0 +1,26 @@
+from math import sqrt
+
+
+class Vector2D:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+    def __add__(self, other):
+        return Vector2D(self.x + other.x, self.y + other.y)
+
+    def __sub__(self, other):
+        return Vector2D(self.x - other.x, self.y - other.y)
+
+    def __str__(self):
+        return f'Vector2D({self.x}, {self.y})'
+
+    def add(self, other):
+        return Vector2D(self.x + other.x, self.y + other.y)
+
+    def sub(self, other):
+        return Vector2D(self.x - other.x, self.y - other.y)
+
+    def distance(self, other):
+        return sqrt((self.x - other.x)**2 + (self.y - other.y)**2)
+
