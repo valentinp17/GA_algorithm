a590a1c6a09e5d26d00593e4549861cee16c09a5
Valentin <pvv377@gmail.com>
2018-12-26 21:35:06 +0300

    Added some functions into vector2d and main

--- main.py
+++ main.py
@@ -2,12 +2,19 @@ import pygame
 import sys
 from pygame import HWSURFACE, DOUBLEBUF, RESIZABLE
 
+from circle import Circle
+from vector2d import Vector2D
+from robot import Robot
 
 COLORS = {
     'black': (0, 0, 0),
     'white': (255, 255, 255),
     'gray': (195, 195, 195),
+    'red': (255, 0, 0),
 }
+COEFFICIENT = 8 # 1 pixel = COEFF
+MAX_F = 1 / 800
+
 
 class Simulation:
     def __init__(self):
@@ -19,7 +26,7 @@ class Simulation:
         pygame.display.set_caption('AG Algorithm')
 
         self.clock = pygame.time.Clock()
-
+        self.robot = Robot(100, 100)
 
         self.init_sim()
 
@@ -29,13 +36,29 @@ class Simulation:
     def init_sim(self):
         pass #TODO
 
-    def update_sim(self):
-        pass #TODO
+    def update_sim(self, circles):
+        self.robot.move(circles, 0.00000000000000001)
+
+    def draw_objects(self, circles):
+        pygame.draw.ellipse(self.screen, COLORS['black'], [self.robot.position.x, self.robot.position.y, 8, 8])
+        print(self.robot, self.robot.get_score())
+
+        for circle in circles:
+            pygame.draw.ellipse(self.screen, COLORS['red'],
+                                [circle.position.x, circle.position.y, circle.radius, circle.radius])
 
     def draw_background(self):
         self.screen.fill(COLORS['white'])
+        pygame.draw.line(self.screen, COLORS['gray'], (100-COEFFICIENT, 100-COEFFICIENT),
+                         (100-COEFFICIENT, 900-COEFFICIENT), 5)
+        pygame.draw.line(self.screen, COLORS['gray'], (100 - COEFFICIENT, 100 - COEFFICIENT),
+                           (900 - COEFFICIENT, 100 - COEFFICIENT), 5)
+        pygame.draw.line(self.screen, COLORS['gray'], (900 - COEFFICIENT, 100 - COEFFICIENT),
+                         (900 - COEFFICIENT, 900 - COEFFICIENT), 5)
+        pygame.draw.line(self.screen, COLORS['gray'], (100 - COEFFICIENT, 900 - COEFFICIENT),
+                         (900 - COEFFICIENT, 900 - COEFFICIENT), 5)
 
-    def run(self):
+    def run(self, circles):
         while True:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
@@ -44,11 +67,16 @@ class Simulation:
                     pass #TODO
 
             self.clock.tick(60)
+            self.update_sim(circles)
             self.draw_background()
-            self.update_sim()
+            self.draw_objects(circles)
+
+
 
             pygame.display.flip()
 
 
 if __name__ == '__main__':
-    Simulation().run()
+    circles = [Circle(500, 500, 100), Circle(700, 700, 100)]
+    Simulation().run(circles)
+
--- vector2d.py
+++ vector2d.py
@@ -1,8 +1,9 @@
-from math import sqrt
+from math import sqrt, acos
 
 
 def normalize(vector):
-    return [float(i)/sum(vector) for i in vector]
+    v_sum = vector.x + vector.y
+    return Vector2D(vector.x / v_sum, vector.y / v_sum)
 
 
 class Vector2D:
@@ -19,6 +20,18 @@ class Vector2D:
     def __str__(self):
         return f'Vector2D({self.x}, {self.y})'
 
+    def __repr__(self):
+        return f'Vector2D({self.x}, {self.y})'
+
+    def __mul__(self, other):
+        types = (int, float)
+        if isinstance(self, types):
+            return Vector2D(self * other.x, self * other.y)
+        elif isinstance(other, types):
+            return Vector2D(self.x * other, self.y * other)
+        else:
+            return Vector2D(self.x * other.x, self.y * other.y)
+
     def add(self, other):
         return Vector2D(self.x + other.x, self.y + other.y)
 
@@ -28,3 +41,9 @@ class Vector2D:
     def distance(self, other):
         return sqrt((self.x - other.x)**2 + (self.y - other.y)**2)
 
+    def get_norm_direction_to(self, vector):
+        dist = self.distance(vector)
+        return Vector2D((vector.x - self.x) / dist, (vector.y - self.y) / dist)
+
+    def get_direction_to(self, vector):
+        return Vector2D((vector.x - self.x), (vector.y - self.y))
